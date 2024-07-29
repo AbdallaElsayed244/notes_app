fc738f704c3e98f0a9f2c1e46ba151b7282c1228
abc <abc@example.com>
2024-07-30 01:04:29 +0300

    created note item

--- lib/widgets/notes_body.dart
+++ lib/widgets/notes_body.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/widgets/custom_app_bar.dart';
+import 'package:todo_app/widgets/note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: const Column(
-        children: [SizedBox(height: 50), CustomAppbar()],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25),
+      child: Column(
+        children: [SizedBox(height: 50), CustomAppbar(),NoteItem()],
       ),
     );
   }
