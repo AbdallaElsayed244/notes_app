0c6a1980498ee70d18d36bc14626eb1112b42cf5
abc <abc@example.com>
2024-07-30 01:20:39 +0300

    floating button

--- lib/view/notes_view.dart
+++ lib/view/notes_view.dart
@@ -6,8 +6,14 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: NotesViewBody(),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add, color: Colors.black),
+        backgroundColor: const Color(0xff4BEADB),
+        shape: CircleBorder(),
+      ),
+      body: const NotesViewBody(),
     );
   }
 }
