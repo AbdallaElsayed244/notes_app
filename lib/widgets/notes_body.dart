6b03398857f0d9e5e89c9416f51d6d3af8daf388
abc <abc@example.com>
2024-07-30 01:12:58 +0300

    created notes listview

--- lib/widgets/notes_body.dart
+++ lib/widgets/notes_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/widgets/custom_app_bar.dart';
 import 'package:todo_app/widgets/note_item.dart';
+import 'package:todo_app/widgets/notes_list.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -10,7 +11,11 @@ class NotesViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 25),
       child: Column(
-        children: [SizedBox(height: 50), CustomAppbar(),NoteItem()],
+        children: [
+          SizedBox(height: 50),
+          CustomAppbar(),
+          Expanded(child: NotesList())
+        ],
       ),
     );
   }
---
+++ lib/widgets/notes_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/widgets/note_item.dart';
+
+class NotesList extends StatelessWidget {
+  const NotesList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (BuildContext context, int index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 8),
+          child: NoteItem(),
+        );
+      },
+    );
+  }
+}
