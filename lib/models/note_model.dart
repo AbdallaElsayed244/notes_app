c40a9149d4c73c812854e4fb8a51baa3103263d8
abc <abc@example.com>
2024-08-03 23:22:30 +0300

    created note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,8 @@
+class NoteModel{
+
+  final String title;
+  final String subtitle;
+  final String date;
+  final String id;
+  NoteModel({required this.title, required this.subtitle, required this.date, required this.id});
+}
